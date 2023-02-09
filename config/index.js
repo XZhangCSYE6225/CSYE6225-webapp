935b8f6560d27b0a6734c55dd71922464ebbfdab
Xiao Zhang <zhang.xiao12@northeastern.edu>
2023-02-09 02:31:56 -0500

    Updated

--- config/index.js
+++ config/index.js
@@ -1,8 +1,15 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
+const {
+    DATABASE_NAME = "user_schema",
+    DATABASE_USERNAME = "root",
+    DATABASE_PASSWORD = "zx991115",
+    DIALECT = "mysql"
+} = process.env;
 
-
-const sequelize = new Sequelize("user_schema", "root", "zx991115", {
-    dialect: 'mysql'
+const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, {
+    dialect: DIALECT
 })
 
 
