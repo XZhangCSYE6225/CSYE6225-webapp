d08cad47d563506672c97d2fe99138b8d58ffcf1
X1aoZhang99 <113070229+X1aoZhang99@users.noreply.github.com>
2023-02-11 15:37:27 -0500

    Update test.js

--- __tests__/test.js
+++ __tests__/test.js
@@ -8,6 +8,6 @@ test('1+1=2', ()=>{
 
 test('Should return a response code of 200', async () => {
     const response = await request("http://localhost:8080").get("/healthz");
-    expect(response.statusCode).toBe(400);
+    expect(response.statusCode).toBe(200);
     await server.close()
 });
