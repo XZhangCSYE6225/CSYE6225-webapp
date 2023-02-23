7748858b3b4a906b8073f17bfc11267fc6ebaa0e
Xiao Zhang <zhang.xiao12@northeastern.edu>
2023-02-22 23:47:37 -0500

    Modified webapp configs

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
 const {
-    DATABASE_NAME = "user_schema",
+    DATABASE_NAME = "webapp",
     DATABASE_USERNAME = "root",
-    DATABASE_PASSWORD = "Zx991115a",
+    DATABASE_PASSWORD = "Zx991115!",
     DIALECT = "mysql"
 } = process.env;
 const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, {
